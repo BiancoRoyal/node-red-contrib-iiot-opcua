acc407f082e26559db128944a1720e758a2704f0
Klaus Landsdorf <klaus.landsdorf@bianco-royal.de>
2017-06-22 11:04:46 +0200

    - discovery lookup could be empty bug fix

--- src/opcua-iiot-connector.js
+++ src/opcua-iiot-connector.js
@@ -126,21 +126,26 @@ module.exports = function (RED) {
         if (err) {
           coreConnector.internalDebugLog('Discovery Error ' + err)
         } else {
-          servers.forEach(function (server) {
-            coreConnector.internalDebugLog('Discovery Server')
-            coreConnector.internalDebugLog('     applicationUri:' + server.applicationUri ? server.applicationUri.cyan.bold : 'none')
-            coreConnector.internalDebugLog('         productUri:' + server.productUri ? server.productUri.cyan.bold : 'none')
-            coreConnector.detailDebugLog('    applicationName:' + server.applicationName ? server.applicationName.text.cyan.bold : 'none')
-            coreConnector.detailDebugLog('               type:' + server.applicationType ? server.applicationType.key.cyan.bold : 'none')
-            coreConnector.detailDebugLog('   gatewayServerUri:' + server.gatewayServerUri ? server.gatewayServerUri.cyan.bold : 'none')
-            coreConnector.detailDebugLog('discoveryProfileUri:' + server.discoveryProfileUri ? server.discoveryProfileUri.cyan.bold : 'none')
-            coreConnector.detailDebugLog('      discoveryUrls:')
+          if (servers && servers.length) {
+            servers.forEach(function (server) {
+              coreConnector.internalDebugLog('Discovery Server')
+              coreConnector.internalDebugLog('     applicationUri:' + server.applicationUri ? server.applicationUri.cyan.bold : 'none')
+              coreConnector.internalDebugLog('         productUri:' + server.productUri ? server.productUri.cyan.bold : 'none')
+              coreConnector.detailDebugLog('    applicationName:' + server.applicationName ? server.applicationName.text.cyan.bold : 'none')
+              coreConnector.detailDebugLog('               type:' + server.applicationType ? server.applicationType.key.cyan.bold : 'none')
+              coreConnector.detailDebugLog('   gatewayServerUri:' + server.gatewayServerUri ? server.gatewayServerUri.cyan.bold : 'none')
+              coreConnector.detailDebugLog('discoveryProfileUri:' + server.discoveryProfileUri ? server.discoveryProfileUri.cyan.bold : 'none')
+              coreConnector.detailDebugLog('      discoveryUrls:')
 
-            server.discoveryUrls.forEach(function (discoveryUrl) {
-              coreConnector.detailDebugLog('                    ' + discoveryUrl.cyan.bold)
+              if (server.discoveryUrls && server.discoveryUrls.length) {
+                server.discoveryUrls.forEach(function (discoveryUrl) {
+                  coreConnector.detailDebugLog('                    ' + discoveryUrl ? discoveryUrl.cyan.bold : 'none')
+                })
+              }
+
+              coreConnector.detailDebugLog('--------------------------------------')
             })
-            coreConnector.detailDebugLog('--------------------------------------')
-          })
+          }
         }
       })
     }
