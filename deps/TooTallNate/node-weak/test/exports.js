069f5064787787d02d74d3af5b35f97dbf7d6ce5
DATATRONiQ Three <ab@datatroniq.com>
2022-05-16 12:31:29 +0200

    package.json: fix node-weak dependency (development only)

---
+++ deps/TooTallNate/node-weak/test/exports.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var weak = require('../')
+
+function checkFunction (prop) {
+  it('should have a function "' + prop + '"', function () {
+    assert('function' == typeof weak[prop]);
+  })
+}
+
+describe('exports', function () {
+
+  afterEach(gc)
+
+  it('should be a function', function () {
+    assert('function' == typeof weak);
+  })
+
+  checkFunction('get')
+  checkFunction('create')
+  checkFunction('isWeakRef')
+  checkFunction('isDead')
+  checkFunction('callbacks')
+  checkFunction('addCallback')
+  checkFunction('removeCallback')
+  checkFunction('removeCallbacks')
+
+  it('should be a circular reference to "create"', function () {
+    assert(weak === weak.create);
+  })
+
+})
