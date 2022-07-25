8514fe688ae626e50d0d631b8a54707d18acce2d
biancode <klaus@bianco-royal.de>
2022-07-25 08:12:36 +0200

    chore(license): update license date and url

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 /*
  The BSD 3-Clause License
 
- Copyright (c) 2018,2019,2020,2021,2022 Klaus Landsdorf (https://bianco-royal.space/)
+ Copyright (c) 2018-2022 Klaus Landsdorf (http://node-red.plus/)
  All rights reserved.
  node-red-contrib-iiot-opcua
  */
