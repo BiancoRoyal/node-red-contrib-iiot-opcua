abfa4b7ffb4a41397f4bade34b37b588637de433
biancode <klaus@bianco-royal.de>
2022-09-28 23:04:52 +0200

    test: silent and more time for crawl

--- jest.config.js
+++ jest.config.js
@@ -183,6 +183,8 @@ module.exports = {
   // Indicates whether each individual test should be reported during the run
   verbose: false,
 
+  silent: true,
+
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
 
